024fcbdba664e9b80e9b11b798f89066759b846a
hilgabe <hilsongabrielcarvalho@gmail.com>
2026-02-11 13:49:16 -0300

    permitir múltiplos responsáveis e edição por marcação
    Substitui seleção única por checkboxes no fluxo de status da OS
    
    Permite adicionar/remover colaboradores no detalhe da OS salvando exatamente os marcados
    
    Pré-carrega colaboradores já registrados como marcados para facilitar manutenção

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.ts7an11dmjg"
+    "revision": "0.2vm2aqq2v1"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
